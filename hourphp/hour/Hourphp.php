ec247c9ddab922402e0e5f634c5a1bd1a992da26
anningri <minganwujian@qq.com>
2018-06-06 07:49:22 +0800

    Add files via upload

--- hourphp/hour/Hourphp.php
+++ hourphp/hour/Hourphp.php
@@ -14,6 +14,58 @@ class Hourphp{
 	public function run()
 	{
 		spl_autoload_register(array($this,'autoLoad'));
+		$this->setReporting();
+		
+	}
+	
+	//删除敏感字符
+	public function stripSlashesDeep($value)
+	{
+		$value = is_array($value) ? array_map(array(this,'stripSlashesDeep'),$value) : stripSlashes($value);
+		return $value;
+	}
+	
+	//检测敏感字符并删除
+	public function removeMagicQuotes()
+	{
+		if(get_magic_quotes_gpc()){
+			$_GET = isset($_GET) ? $this->stripSlashesDeep($_GET ) : '';
+            $_POST = isset($_POST) ? $this->stripSlashesDeep($_POST ) : '';
+            $_COOKIE = isset($_COOKIE) ? $this->stripSlashesDeep($_COOKIE) : '';
+            $_SESSION = isset($_SESSION) ? $this->stripSlashesDeep($_SESSION) : '';
+		}
+	}
+	
+	/**检测自定义全局变量并移除。因为 register_globals 已经弃用，如果
+     * 已经弃用的 register_globals 指令被设置为 on，那么局部变量也将
+     * 在脚本的全局作用域中可用。 例如， $_POST['foo'] 也将以 $foo 的
+     * 形式存在，这样写是不好的实现，会影响代码中的其他变量。 相关信息，
+     * 参考: http://php.net/manual/zh/faq.using.php#faq.register-globals
+	*/
+	public function unregisterGlobals()
+	{
+		if(ini_get('register_globals')){
+			$array = array('_SESSION', '_POST', '_GET', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES');
+			foreach($array as $value){
+				if($_GLOBALS[$value]){
+					unset($_GLOBALS[$value]);
+				}
+			}
+		}
+	}
+	
+	//检测开发环境
+	public function setReporting()
+	{
+		if(APP_DEBUG === true){
+			error_reporting(E_ALL);
+			ini_set('display_errors','on');
+		}else{
+			error_reporting(E_ALL);
+			ini_set('display_errors','on');
+			ini_set('log_errors','on');
+		}
+		
 	}
 	
 	public static function autoLoad($class)
